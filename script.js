b01152a742be22a83e3a394aa5c50b1ecc499faf
Elmer Herrera <dcshoeco109790@yahoo.com>
2022-12-27 19:46:14 -0500

    added buttons fuctioning questions and score

--- script.js
+++ script.js
@@ -35,3 +35,64 @@ const question5 = new Question (
     ['var x = "true"', 'x = TRUE', 'var x = "false"', 'var x = false'],
     'var x = false'
 )
+
+class Quiz {
+    constructor(questions) {
+      this.questions = questions;
+      this.currentIndex = 0;
+    }
+
+    getCurrentQuestion() {
+      return this.questions[this.currentIndex];
+    }
+
+    nextQuestion() {
+      this.currentIndex++;
+    }
+
+    hasEnded() {
+      return this.currentIndex >= this.questions.length;
+    }
+  }
+
+  let quiz = new Quiz([question1, question2, question3, question4, question5])
+
+  function showCurrentQuestion() {
+    const question = quiz.getCurrentQuestion();
+    const questionElement = document.getElementById("question");
+    questionElement.innerText = question.question;
+
+    const choices = question.choices;
+    for (let i = 0; i < choices.length; i++) {
+      const choiceElement = document.getElementById(`choice${i}`);
+      choiceElement.innerText = choices[i];
+    }
+  }
+
+  let score = 0;
+
+  function checkAnswer(selectedChoice) {
+    if (selectedChoice === quiz.getCurrentQuestion().answer) {
+      score += 12.5;
+    } else {
+      score -= 12.5;
+    }
+    showNextQuestion();
+    updateScore();
+  }
+
+  function updateScore() {
+    const scoreElement = document.getElementById("score");
+    scoreElement.innerText = `Score: ${score}`;
+  }
+
+  function showNextQuestion() {
+    quiz.nextQuestion();
+    if (quiz.hasEnded()) {
+      alert("Quiz has ended!");
+      return;
+    }
+    showCurrentQuestion();
+  }
+
+  showCurrentQuestion();
